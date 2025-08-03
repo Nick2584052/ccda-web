656f57b504fd793bc9350d5429a554e76e218ba8
Nick2584052 <nick2584052@gmail.com>
2025-08-04 00:41:39 +0800

    Add files via upload

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://kkpujgndxgwwxywrilbu.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
